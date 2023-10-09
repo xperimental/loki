7716182f1d6273ca6bb0c793db042513d1cc5dd8
Robert Jacob <rojacob@redhat.com>
2023-10-09 21:04:54 +0200

    Move query-frontend to common deployment

--- operator/internal/manifests/query-frontend.go
+++ operator/internal/manifests/query-frontend.go
@@ -1,19 +1,12 @@
 package manifests
 
 import (
-	"fmt"
-	"path"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	policyv1 "k8s.io/api/policy/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	"github.com/grafana/loki/operator/internal/manifests/internal/config"
 )
 
 // BuildQueryFrontend returns a list of k8s objects for Loki QueryFrontend
@@ -66,114 +59,7 @@ func BuildQueryFrontend(opts Options) ([]client.Object, error) {
 
 // NewQueryFrontendDeployment creates a deployment object for a query-frontend
 func NewQueryFrontendDeployment(opts Options) *appsv1.Deployment {
-	l := ComponentLabels(LabelQueryFrontendComponent, opts.Name)
-	a := commonAnnotations(opts.ConfigSHA1, opts.ObjectStorage.SecretSHA1, opts.CertRotationRequiredAt)
-	podSpec := corev1.PodSpec{
-		Affinity: configureAffinity(LabelQueryFrontendComponent, opts.Name, opts.Gates.DefaultNodeAffinity, opts.Stack.Template.QueryFrontend),
-		Volumes: []corev1.Volume{
-			{
-				Name: configVolumeName,
-				VolumeSource: corev1.VolumeSource{
-					ConfigMap: &corev1.ConfigMapVolumeSource{
-						DefaultMode: &defaultConfigMapMode,
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: lokiConfigMapName(opts.Name),
-						},
-					},
-				},
-			},
-		},
-		Containers: []corev1.Container{
-			{
-				Image: opts.Image,
-				Name:  lokiFrontendContainerName,
-				Resources: corev1.ResourceRequirements{
-					Limits:   opts.ResourceRequirements.QueryFrontend.Limits,
-					Requests: opts.ResourceRequirements.QueryFrontend.Requests,
-				},
-				Args: []string{
-					"-target=query-frontend",
-					fmt.Sprintf("-config.file=%s", path.Join(config.LokiConfigMountDir, config.LokiConfigFileName)),
-					fmt.Sprintf("-runtime-config.file=%s", path.Join(config.LokiConfigMountDir, config.LokiRuntimeConfigFileName)),
-					"-config.expand-env=true",
-				},
-				ReadinessProbe: &corev1.Probe{
-					ProbeHandler: corev1.ProbeHandler{
-						HTTPGet: &corev1.HTTPGetAction{
-							// The frontend will only return ready once a querier has connected to it.
-							// Because the service used for connecting the querier to the frontend only lists ready
-							// instances there's sequencing issue. For now, we re-use the liveness-probe path
-							// for the readiness-probe as a workaround.
-							Path:   lokiLivenessPath,
-							Port:   intstr.FromInt(httpPort),
-							Scheme: corev1.URISchemeHTTP,
-						},
-					},
-					PeriodSeconds:       10,
-					InitialDelaySeconds: 15,
-					TimeoutSeconds:      1,
-					SuccessThreshold:    1,
-					FailureThreshold:    3,
-				},
-				LivenessProbe: lokiLivenessProbe(),
-				Ports: []corev1.ContainerPort{
-					{
-						Name:          lokiHTTPPortName,
-						ContainerPort: httpPort,
-						Protocol:      protocolTCP,
-					},
-					{
-						Name:          lokiGRPCPortName,
-						ContainerPort: grpcPort,
-						Protocol:      protocolTCP,
-					},
-				},
-				VolumeMounts: []corev1.VolumeMount{
-					{
-						Name:      configVolumeName,
-						ReadOnly:  false,
-						MountPath: config.LokiConfigMountDir,
-					},
-				},
-				TerminationMessagePath:   "/dev/termination-log",
-				TerminationMessagePolicy: "File",
-				ImagePullPolicy:          "IfNotPresent",
-			},
-		},
-	}
-
-	if opts.Stack.Template != nil && opts.Stack.Template.QueryFrontend != nil {
-		podSpec.Tolerations = opts.Stack.Template.QueryFrontend.Tolerations
-		podSpec.NodeSelector = opts.Stack.Template.QueryFrontend.NodeSelector
-	}
-
-	return &appsv1.Deployment{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Deployment",
-			APIVersion: appsv1.SchemeGroupVersion.String(),
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   QueryFrontendName(opts.Name),
-			Labels: l,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: pointer.Int32(opts.Stack.Template.QueryFrontend.Replicas),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: labels.Merge(l, GossipLabels()),
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:        fmt.Sprintf("%s-%s", lokiFrontendContainerName, opts.Name),
-					Labels:      labels.Merge(l, GossipLabels()),
-					Annotations: a,
-				},
-				Spec: podSpec,
-			},
-			Strategy: appsv1.DeploymentStrategy{
-				Type: appsv1.RollingUpdateDeploymentStrategyType,
-			},
-		},
-	}
+	return newDeployment(opts, LabelQueryFrontendComponent)
 }
 
 // NewQueryFrontendGRPCService creates a k8s service for the query-frontend GRPC endpoint
