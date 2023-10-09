41508394c1f4447019ef41ce60c08e72a657c65b
Robert Jacob <rojacob@redhat.com>
2023-10-09 20:58:42 +0200

    Move querier into common deployment

--- operator/internal/manifests/querier.go
+++ operator/internal/manifests/querier.go
@@ -1,20 +1,14 @@
 package manifests
 
 import (
-	"fmt"
-	"math"
-	"path"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	policyv1 "k8s.io/api/policy/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/utils/pointer"
+	"math"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/grafana/loki/operator/internal/manifests/internal/config"
 	"github.com/grafana/loki/operator/internal/manifests/storage"
 )
 
@@ -72,102 +66,7 @@ func BuildQuerier(opts Options) ([]client.Object, error) {
 
 // NewQuerierDeployment creates a deployment object for a querier
 func NewQuerierDeployment(opts Options) *appsv1.Deployment {
-	l := ComponentLabels(LabelQuerierComponent, opts.Name)
-	a := commonAnnotations(opts.ConfigSHA1, opts.ObjectStorage.SecretSHA1, opts.CertRotationRequiredAt)
-	podSpec := corev1.PodSpec{
-		Affinity: configureAffinity(LabelQuerierComponent, opts.Name, opts.Gates.DefaultNodeAffinity, opts.Stack.Template.Querier),
-		Volumes: []corev1.Volume{
-			{
-				Name: configVolumeName,
-				VolumeSource: corev1.VolumeSource{
-					ConfigMap: &corev1.ConfigMapVolumeSource{
-						DefaultMode: &defaultConfigMapMode,
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: lokiConfigMapName(opts.Name),
-						},
-					},
-				},
-			},
-		},
-		Containers: []corev1.Container{
-			{
-				Image: opts.Image,
-				Name:  "loki-querier",
-				Resources: corev1.ResourceRequirements{
-					Limits:   opts.ResourceRequirements.Querier.Limits,
-					Requests: opts.ResourceRequirements.Querier.Requests,
-				},
-				Args: []string{
-					"-target=querier",
-					fmt.Sprintf("-config.file=%s", path.Join(config.LokiConfigMountDir, config.LokiConfigFileName)),
-					fmt.Sprintf("-runtime-config.file=%s", path.Join(config.LokiConfigMountDir, config.LokiRuntimeConfigFileName)),
-					"-config.expand-env=true",
-				},
-				ReadinessProbe: lokiReadinessProbe(),
-				LivenessProbe:  lokiLivenessProbe(),
-				Ports: []corev1.ContainerPort{
-					{
-						Name:          lokiHTTPPortName,
-						ContainerPort: httpPort,
-						Protocol:      protocolTCP,
-					},
-					{
-						Name:          lokiGRPCPortName,
-						ContainerPort: grpcPort,
-						Protocol:      protocolTCP,
-					},
-					{
-						Name:          lokiGossipPortName,
-						ContainerPort: gossipPort,
-						Protocol:      protocolTCP,
-					},
-				},
-				VolumeMounts: []corev1.VolumeMount{
-					{
-						Name:      configVolumeName,
-						ReadOnly:  false,
-						MountPath: config.LokiConfigMountDir,
-					},
-				},
-				TerminationMessagePath:   "/dev/termination-log",
-				TerminationMessagePolicy: "File",
-				ImagePullPolicy:          "IfNotPresent",
-			},
-		},
-	}
-
-	if opts.Stack.Template != nil && opts.Stack.Template.Querier != nil {
-		podSpec.Tolerations = opts.Stack.Template.Querier.Tolerations
-		podSpec.NodeSelector = opts.Stack.Template.Querier.NodeSelector
-	}
-
-	return &appsv1.Deployment{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Deployment",
-			APIVersion: appsv1.SchemeGroupVersion.String(),
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   QuerierName(opts.Name),
-			Labels: l,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: pointer.Int32(opts.Stack.Template.Querier.Replicas),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: labels.Merge(l, GossipLabels()),
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:        fmt.Sprintf("loki-querier-%s", opts.Name),
-					Labels:      labels.Merge(l, GossipLabels()),
-					Annotations: a,
-				},
-				Spec: podSpec,
-			},
-			Strategy: appsv1.DeploymentStrategy{
-				Type: appsv1.RollingUpdateDeploymentStrategyType,
-			},
-		},
-	}
+	return newDeployment(opts, LabelQuerierComponent)
 }
 
 // NewQuerierGRPCService creates a k8s service for the querier GRPC endpoint
