6d2461f51f272176df9cd2af1e6d314eba159f73
Robert Jacob <rojacob@redhat.com>
2023-10-10 18:37:43 +0200

    Move ingester to common statefulset

--- operator/internal/manifests/ingester.go
+++ operator/internal/manifests/ingester.go
@@ -1,20 +1,13 @@
 package manifests
 
 import (
-	"fmt"
-	"path"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	policyv1 "k8s.io/api/policy/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/grafana/loki/operator/internal/manifests/internal/config"
 	"github.com/grafana/loki/operator/internal/manifests/storage"
 )
 
@@ -72,151 +65,7 @@ func BuildIngester(opts Options) ([]client.Object, error) {
 
 // NewIngesterStatefulSet creates a deployment object for an ingester
 func NewIngesterStatefulSet(opts Options) *appsv1.StatefulSet {
-	l := ComponentLabels(LabelIngesterComponent, opts.Name)
-	a := commonAnnotations(opts.ConfigSHA1, opts.ObjectStorage.SecretSHA1, opts.CertRotationRequiredAt)
-	podSpec := corev1.PodSpec{
-		Affinity: configureAffinity(LabelIngesterComponent, opts.Name, opts.Gates.DefaultNodeAffinity, opts.Stack.Template.Ingester),
-		Volumes: []corev1.Volume{
-			{
-				Name: configVolumeName,
-				VolumeSource: corev1.VolumeSource{
-					ConfigMap: &corev1.ConfigMapVolumeSource{
-						DefaultMode: &defaultConfigMapMode,
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: lokiConfigMapName(opts.Name),
-						},
-					},
-				},
-			},
-		},
-		Containers: []corev1.Container{
-			{
-				Image: opts.Image,
-				Name:  "loki-ingester",
-				Resources: corev1.ResourceRequirements{
-					Limits:   opts.ResourceRequirements.Ingester.Limits,
-					Requests: opts.ResourceRequirements.Ingester.Requests,
-				},
-				Args: []string{
-					"-target=ingester",
-					fmt.Sprintf("-config.file=%s", path.Join(config.LokiConfigMountDir, config.LokiConfigFileName)),
-					fmt.Sprintf("-runtime-config.file=%s", path.Join(config.LokiConfigMountDir, config.LokiRuntimeConfigFileName)),
-					"-config.expand-env=true",
-				},
-				ReadinessProbe: lokiReadinessProbe(),
-				LivenessProbe:  lokiLivenessProbe(),
-				Ports: []corev1.ContainerPort{
-					{
-						Name:          lokiHTTPPortName,
-						ContainerPort: httpPort,
-						Protocol:      protocolTCP,
-					},
-					{
-						Name:          lokiGRPCPortName,
-						ContainerPort: grpcPort,
-						Protocol:      protocolTCP,
-					},
-					{
-						Name:          lokiGossipPortName,
-						ContainerPort: gossipPort,
-						Protocol:      protocolTCP,
-					},
-				},
-				VolumeMounts: []corev1.VolumeMount{
-					{
-						Name:      configVolumeName,
-						ReadOnly:  false,
-						MountPath: config.LokiConfigMountDir,
-					},
-					{
-						Name:      storageVolumeName,
-						ReadOnly:  false,
-						MountPath: dataDirectory,
-					},
-					{
-						Name:      walVolumeName,
-						ReadOnly:  false,
-						MountPath: walDirectory,
-					},
-				},
-				TerminationMessagePath:   "/dev/termination-log",
-				TerminationMessagePolicy: "File",
-				ImagePullPolicy:          "IfNotPresent",
-			},
-		},
-	}
-
-	if opts.Stack.Template != nil && opts.Stack.Template.Ingester != nil {
-		podSpec.Tolerations = opts.Stack.Template.Ingester.Tolerations
-		podSpec.NodeSelector = opts.Stack.Template.Ingester.NodeSelector
-	}
-
-	return &appsv1.StatefulSet{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "StatefulSet",
-			APIVersion: appsv1.SchemeGroupVersion.String(),
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   IngesterName(opts.Name),
-			Labels: l,
-		},
-		Spec: appsv1.StatefulSetSpec{
-			PodManagementPolicy:  appsv1.OrderedReadyPodManagement,
-			RevisionHistoryLimit: pointer.Int32(10),
-			Replicas:             pointer.Int32(opts.Stack.Template.Ingester.Replicas),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: labels.Merge(l, GossipLabels()),
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:        fmt.Sprintf("loki-ingester-%s", opts.Name),
-					Labels:      labels.Merge(l, GossipLabels()),
-					Annotations: a,
-				},
-				Spec: podSpec,
-			},
-			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
-				{
-					ObjectMeta: metav1.ObjectMeta{
-						Labels: l,
-						Name:   storageVolumeName,
-					},
-					Spec: corev1.PersistentVolumeClaimSpec{
-						AccessModes: []corev1.PersistentVolumeAccessMode{
-							// TODO: should we verify that this is possible with the given storage class first?
-							corev1.ReadWriteOnce,
-						},
-						Resources: corev1.ResourceRequirements{
-							Requests: map[corev1.ResourceName]resource.Quantity{
-								corev1.ResourceStorage: opts.ResourceRequirements.Ingester.PVCSize,
-							},
-						},
-						StorageClassName: pointer.String(opts.Stack.StorageClassName),
-						VolumeMode:       &volumeFileSystemMode,
-					},
-				},
-				{
-					ObjectMeta: metav1.ObjectMeta{
-						Labels: l,
-						Name:   walVolumeName,
-					},
-					Spec: corev1.PersistentVolumeClaimSpec{
-						AccessModes: []corev1.PersistentVolumeAccessMode{
-							// TODO: should we verify that this is possible with the given storage class first?
-							corev1.ReadWriteOnce,
-						},
-						Resources: corev1.ResourceRequirements{
-							Requests: map[corev1.ResourceName]resource.Quantity{
-								corev1.ResourceStorage: opts.ResourceRequirements.WALStorage.PVCSize,
-							},
-						},
-						StorageClassName: pointer.String(opts.Stack.StorageClassName),
-						VolumeMode:       &volumeFileSystemMode,
-					},
-				},
-			},
-		},
-	}
+	return newStatefulSet(opts, LabelIngesterComponent)
 }
 
 // NewIngesterGRPCService creates a k8s service for the ingester GRPC endpoint
