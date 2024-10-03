de4a0f2af179957c63859e792b65c63cfe41d286
Robert Jacob <rojacob@redhat.com>
2024-10-03 13:30:51 +0200

    Add openshift-logging defaults

--- operator/internal/manifests/config.go
+++ operator/internal/manifests/config.go
@@ -397,11 +397,68 @@ func retentionConfig(ls *lokiv1.LokiStackSpec) config.RetentionOptions {
 	}
 }
 
+func defaultOTLPAttrbuteConfig(ts *lokiv1.TenantsSpec) config.OTLPAttributeConfig {
+	if ts == nil || ts.Mode != lokiv1.OpenshiftLogging {
+		return config.OTLPAttributeConfig{}
+	}
+
+	result := config.OTLPAttributeConfig{
+		DefaultIndexLabels: []string{
+			"log_type",
+			"kubernetes_namespace_name",
+			"kubernetes_pod_name",
+			"kubernetes_container_name",
+			"k8s.namespace.name",
+		},
+	}
+
+	if ts.Openshift == nil || ts.Openshift.OTLP == nil || !ts.Openshift.OTLP.DisableRecommendedLabels {
+		result.DefaultIndexLabels = append(result.DefaultIndexLabels, []string{
+			"service.name",
+			"k8s.cluster.uid",
+			"k8s.pod.name",
+			"k8s.container.name",
+			"openshift.log.source",
+			"k8s.deployment.name",
+			"k8s.statefulset.name",
+			"k8s.daemonset.name",
+			"k8s.cronjob.name",
+		}...)
+
+		result.Global = &config.OTLPTenantAttributeConfig{
+			ResourceAttributes: []config.OTLPAttribute{
+				{
+					Action: config.OTLPAttributeActionMetadata,
+					Names: []string{
+						"k8s.replicaset.name",
+						"k8s.job.name",
+					},
+				},
+			},
+			LogAttributes: []config.OTLPAttribute{
+				{
+					Action: config.OTLPAttributeActionMetadata,
+					Names: []string{
+						"k8s.node.name",
+						"service.name",
+						"host.name",
+						"process.command",
+						"node.name",
+						"url.path",
+						"http.request.method_original",
+						"http.response.status.code",
+					},
+				},
+			},
+		}
+	}
+
+	return result
+}
+
 func otlpAttributeConfig(ls *lokiv1.LokiStackSpec) config.OTLPAttributeConfig {
 	// TODO provide default stream labels
-	result := config.OTLPAttributeConfig{
-		DefaultIndexLabels: []string{},
-	}
+	result := defaultOTLPAttrbuteConfig(ls.Tenants)
 
 	if ls.Limits != nil {
 		if ls.Limits.Global != nil && ls.Limits.Global.OTLP != nil {
